c0429e24ba2220accec3fe71c9f2cb6cb7727b2a
mcustodio <mauricioramalho2@gmail.com>
2018-06-06 07:20:38 -0300

    MapFragment: Temporary pin is now fixed to center of screen.

--- app/src/main/java/com/mcustodio/parknow/view/main/map/MapFragment.kt
+++ app/src/main/java/com/mcustodio/parknow/view/main/map/MapFragment.kt
@@ -11,8 +11,10 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.mcustodio.parknow.view.main.MainViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.Marker
-import com.mcustodio.parknow.debug
 import com.mcustodio.parknow.view.edit.EditActivity
+import com.google.android.gms.maps.model.LatLngBounds
+
+
 
 
 class MapFragment : SupportMapFragment() {
@@ -29,15 +31,16 @@ class MapFragment : SupportMapFragment() {
         super.onCreate(p0)
         getMapAsync { map ->
             this.map = map
-            this.map?.setOnMarkerDragListener(onDragListener)
             this.map?.uiSettings?.isZoomControlsEnabled = true
             setupViewModel()
+            map?.setOnMarkerClickListener { viewModel.isCreationMode.value == true }    // false = usa comportamento default
+            map?.setOnCameraMoveListener { movablePin?.position = this@MapFragment.map?.cameraPosition?.target }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        pins = null     // Para não vazar memória
+        pins = null     // Evitar memory leak
     }
 
     private fun setupViewModel() {
@@ -45,53 +48,35 @@ class MapFragment : SupportMapFragment() {
             pins = data?.map {
                 val pos = LatLng(it.latitude!!, it.longitude!!)
                 MarkerOptions()
-                        .position(pos)
-                        .title(it.name)
+                    .position(pos)
+                    .title(it.name)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
             }
             showParkingLots()
         })
 
         viewModel.isCreationMode.observe(this, Observer {
-            it?.let {
-                if (it == true) {
-                    map?.clear()
-                    map?.setOnMapClickListener { moveMarker(it)}
-                    map?.setOnMapLongClickListener { moveMarker(it) }
-                    map?.setOnMarkerClickListener { true }
-                    movablePin = map?.addMarker(movableMarker(LatLng(0.0,0.0)))
-                } else {
-                    movablePin = null
-                    showParkingLots()
-                }
+            if (it == true) {
+                map?.clear()
+                movablePin = map?.addMarker(MarkerOptions().position(map!!.cameraPosition.target))
+            } else {
+                movablePin = null
+                showParkingLots()
             }
         })
     }
 
     private fun showParkingLots() {
         map?.clear()
-        pins?.forEach { map?.addMarker(it) }
-    }
-
-    private fun moveMarker(pos: LatLng) {
-        movablePin?.position = pos
-        map?.animateCamera(CameraUpdateFactory.newLatLng(pos))
-    }
-
-    private fun movableMarker(pos: LatLng) : MarkerOptions {
-        return MarkerOptions()
-                .position(pos)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+        val markers = pins?.map { map?.addMarker(it) }  // Add markers to map and save to variable
+        val builder = LatLngBounds.Builder()
+        markers?.filterNotNull()?.forEach { builder.include(it.position) }
+        val bounds = builder.build()
+        val camera = CameraUpdateFactory.newLatLngBounds(bounds, 15)
+        map?.animateCamera(camera)
     }
 
     fun launchEditActivity() {
         movablePin?.position?.let { EditActivity.launchNew(activity!!, it) }
-    }
-
-    private val onDragListener = object : GoogleMap.OnMarkerDragListener {
-        override fun onMarkerDragStart(p0: Marker?) {}
-        override fun onMarkerDrag(p0: Marker?) {}
-        override fun onMarkerDragEnd(p0: Marker?) {
-            map?.animateCamera(CameraUpdateFactory.newLatLng(p0?.position))
-        }
     }
 }
